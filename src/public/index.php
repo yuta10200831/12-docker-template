ff8dfc8cfc427cb72149ac35f1ba97d4e4c3ee41
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-17 21:50:40 +0900

    課題①提出

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,20 @@
 <?php
-echo 'Welcome TECH QUEST!';
+$examScoreList = [
+	"国語" => 70,
+	"算数" => 31,
+	"理科" => 45,
+	"社会" => 29,
+	"英語" => 30,
+];
+
+$resultList = [];
+
+foreach ($examScoreList as $subject => $score) {
+  if ($score >= 30) {
+    $resultList[] = $subject . "は合格";
+  }else {
+    $resultList[] = $subject . "は不合格";
+  }
+}
+
+echo implode("<br>", $resultList);
